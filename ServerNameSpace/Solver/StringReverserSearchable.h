3dd90f3eb277222f54b209acfa89f6ad7e4fcc6c
Stav <stavkoren123@gmail.com>
2019-01-16 01:38:36 +0200

    more comments

--- ServerNameSpace/Solver/StringReverserSearchable.h
+++ ServerNameSpace/Solver/StringReverserSearchable.h
@@ -14,18 +14,34 @@ public:
             this->problem+= pro[i];
         }
     }
+    /**
+     * GetInitialState
+     * @return  shared ptr of initial state
+     */
     shared_ptr<State<string>> GetInitialState(){
         return make_shared<State<string>>(this->problem);
     }
-    //TODO: understand how to implement is goal for string
+    /**
+     * IsGoalState
+     * @param state
+     * @return true if its goal state or false otherwise
+     */
     bool IsGoalState(shared_ptr<State<string>> state){
     return true;
     }
+    /**
+     * GetAllPossiableStates
+     * @param s
+     * @return all possiable states of string
+     */
     vector<shared_ptr<State<string>>>GetAllPossiableStates(shared_ptr<State<string>> s){
         vector<shared_ptr<State<string>>> vec;
         return vec;
     }
-
+/**
+ * ToString
+ * @return problem at string representation
+ */
     string ToString(){
         return problem;
     }
